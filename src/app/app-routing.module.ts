3b2789346fe224e56188ae26a93971d26c9d8503
fatarazonac <95062150+fatarazonac@users.noreply.github.com>
2021-11-25 22:38:59 -0500

    1.0
    Proyecto G13

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component'
+
+const routes: Routes = [
+  {
+    path:"header",
+    component:HeaderComponent
+  },
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"footer",
+    component:FooterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
